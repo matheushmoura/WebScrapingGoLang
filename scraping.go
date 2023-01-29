318dae1423f1b530835c3f6c3895bf8b94690384
matheus <matheusshutt2@gmail.com>
2023-01-29 19:00:56 -0300

    scraping.go

--- scraping.go
+++ scraping.go
@@ -50,6 +50,41 @@ func FileToDataframe(filepath string) (dataframe.DataFrame, error) {
 	return df, nil
 }
 
+func DirectToDownload(linkDownload string) {
+	resDownload, erroDownload := goquery.NewDocument(linkDownload)
+	if erroDownload != nil {
+		log.Fatal(erroDownload)
+	}
+	downloadButton, existsButton := resDownload.Find("a:has(i[class='fa fa-arrow-circle-o-down'])").Attr("href")
+	if existsButton {
+		parts := strings.Split(downloadButton, "/")
+		fileUrl := parts[len(parts)-1]
+		fileUrl = "tmp/" + fileUrl
+
+		err := DownloadFile(fileUrl, downloadButton)
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("Link:", downloadButton, " \nArquivo Baixado:", fileUrl)
+
+			df, erro := FileToDataframe(fileUrl)
+			if erro != nil {
+				panic(err)
+			} else {
+				// Mostrar a tabela
+				fmt.Println(df)
+			}
+
+			err := os.Remove(fileUrl)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println("Arquivo Removido: " + fileUrl)
+		}
+	}
+}
+
 func main() {
 	res, err := goquery.NewDocument("https://repositorio.seade.gov.br/dataset/municipios")
 	if err != nil {
@@ -63,38 +98,7 @@ func main() {
 		link, exists := s.Attr("href")
 		if exists {
 			linkdownload := "https://repositorio.seade.gov.br" + link
-			resDownload, erroDownload := goquery.NewDocument(linkdownload)
-			if erroDownload != nil {
-				log.Fatal(erroDownload)
-			}
-			downloadButton, existsButton := resDownload.Find("a:has(i[class='fa fa-arrow-circle-o-down'])").Attr("href")
-			if existsButton {
-				parts := strings.Split(downloadButton, "/")
-				fileUrl := parts[len(parts)-1]
-				fileUrl = "tmp/" + fileUrl
-
-				err := DownloadFile(fileUrl, downloadButton)
-				if err != nil {
-					panic(err)
-				} else {
-					fmt.Println("Link:", downloadButton, " \nArquivo Baixado:", fileUrl)
-
-					df, erro := FileToDataframe(fileUrl)
-					if erro != nil {
-						panic(err)
-					} else {
-						// Mostrar a tabela
-						fmt.Println(df)
-					}
-
-					err := os.Remove(fileUrl)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-					fmt.Println("Arquivo Removido: " + fileUrl)
-				}
-			}
+			DirectToDownload(linkdownload)
 		}
 	})
 }
